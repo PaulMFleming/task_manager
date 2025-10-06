c476fe79fa9de9e517208787474bba8b3dc0e75f
Paul Fleming <paul.stillbreathing@gmail.com>
2025-10-06 10:13:00 +0100

    add task_list spec

---
+++ spec/task_list_spec.rb
@@ -0,0 +1,31 @@
+require 'date'
+require_relative '../task'
+require_relative './task_list'
+
+RSpec.describe TaskList do
+  let(:task1) { Task.new(title: 'Task 1', due_date: Date.today -2)}
+  let(:task2) { Task.new(title: 'Task 2', due_date: Date.today +1)}
+  let(:task3) { Task.new(title: 'Task 3', due_date: nil)}
+  subject { TaskList.new([task1, task2, task3]) }
+
+  it 'returns all tasks' do
+    expect(subject.all).to eq([task1, task2, task3])
+    expect(subject.all.size).to eq(3)
+  end
+
+  it 'returns all tasks' do
+    expect(subject.all).to eq([task1, task2, task3])
+    expect(subject.all.size).to eq(3)
+  end
+
+  it 'adds a new task' do
+    new_task = Task.new(title: 'Task 4')
+    subject.add(new_task)
+    expect(subject.all.size).to eq(4)
+    expect(subject.all).to include(new_task)
+  end
+
+  it 'returns overdue tasks' do
+    expect(subject.overdue).to eq([task1])
+  end
+end
