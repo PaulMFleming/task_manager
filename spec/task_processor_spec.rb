d89b01ea435ca37d85dbfb479e7f53a5ad228950
Paul Fleming <paul.stillbreathing@gmail.com>
2025-10-21 14:21:34 +0100

    add specs for TaskProcessor class - they'll fail currently

---
+++ spec/task_processor_spec.rb
@@ -0,0 +1,25 @@
+
+require_relative '../task'
+require_relative '../task_list'
+require_relative '../task_processor'
+
+Rspec.describe TaskProcessor do
+  let(:task1) { Task.new(title: 'Build feature') }
+  let(:task2) { Task.new(title: 'Write tests') }
+  let(:list) { TaskList.new([task1, task2]) }
+
+  subject { TaskProcessor.new(list) }
+
+  describe '#process_all' do
+    it 'starts all pending tasks and marks them as in progress' do
+      subject.process_all
+      expect(list.by_state('inprogress').size).to eq(2)
+    end
+
+    it 'can complete all in-progress tasks' do
+      list.complete_all_pending
+      subject.complete_all
+      expect(list.by_state('completed').size).to eq(2)
+    end
+  end
+end
